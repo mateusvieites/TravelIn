f129ca4e8e0a2a01a812e0dcc5a5bab56906295c
HardszVick <48382353+mateusvieites@users.noreply.github.com>
2021-10-09 12:17:56 -0300

    Adicionado padronização de formulário

--- app/src/main/java/dev/faculdade/travelin/MenuActivity.java
+++ app/src/main/java/dev/faculdade/travelin/MenuActivity.java
@@ -2,13 +2,39 @@ package dev.faculdade.travelin;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import dev.faculdade.travelin.DataBase.dao.LoginDAO;
 
 public class MenuActivity extends AppCompatActivity {
+    private Button btPlanejarViagem,btMinhasViagens;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu);
+
+
+        btPlanejarViagem = findViewById(R.id.btPlanejar);
+        btMinhasViagens = findViewById(R.id.btMinhasViagens);
+
+        btPlanejarViagem.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //Abrir menu para cadastrar nova pesquisa de viagem
+                //startActivity(new Intent( MainActivity.this, MenuActivity.class));
+            }
+        });
+
+        btMinhasViagens.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //Abrir menu de pesquisa de viagens
+                //startActivity(new Intent( MainActivity.this, MenuActivity.class));
+            }
+        });
     }
 }
